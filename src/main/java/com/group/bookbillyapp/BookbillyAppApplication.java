100960266c26601a375d8bc878df09f0403bece3
charlesuu <tiger0192004@gmail.com>
2023-03-31 19:02:07 +0900

    feat: setup project

---
+++ src/main/java/com/group/bookbillyapp/BookbillyAppApplication.java
@@ -0,0 +1,13 @@
+package com.group.bookbillyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookbillyAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookbillyAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/group/bookbillyapp/BookbillyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group.bookbillyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookbillyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
